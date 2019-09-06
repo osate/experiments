b388d791fa86fd4ef476dca0637b1706bc8353ff
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2019-09-06 16:29:41 -0400

    New plugin for Awas verification methods.

---
+++ isse/org.osate.isse.verificationmethods/src/org/osate/isse/verificationmethods/AwasMethods.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2004-2017 Carnegie Mellon University and others.
+ * (see Contributors file).
+ *   
+ * All Rights Reserved.
+ * 
+ * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON
+ * UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS
+ * TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
+ * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE
+ * MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND
+ * WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Created, in part, with funding and support from the United States Government.
+ * (see Acknowledgments file).
+ * 
+ * This program includes and/or can make use of certain third party source code,
+ * object code, documentation and other files ("Third Party Software"). The
+ * Third Party Software that is used by this program is dependent upon your
+ * system configuration. By using this program, You agree to comply with any and
+ * all relevant Third Party Software terms and conditions contained in any such
+ * Third Party Software or separate license file distributed with such Third
+ * Party Software. The parties who own the Third Party Software ("Third Party
+ * Licensors") are intended third party beneficiaries to this License with
+ * respect to the terms applicable to their Third Party Software. Third Party
+ * Software licenses only apply to the Third Party Software and not any other
+ * portion of this program or this program as a whole.
+ */
+package org.osate.isse.verificationmethods;
+
+import org.osate.aadl2.instance.ComponentInstance;
+import org.sireum.aadl.osate.util.Util;
+import org.sireum.awas.AADLBridge.AadlHandler;
+import org.sireum.awas.ast.Model;
+import org.sireum.awas.awasfacade.AwasGraphImpl;
+import org.sireum.awas.fptc.FlowEdge;
+import org.sireum.awas.fptc.FlowGraph;
+import org.sireum.awas.fptc.FlowNode;
+import org.sireum.awas.symbol.SymbolTable;
+import org.sireum.util.ConsoleTagReporter;
+
+public class AwasMethods {
+	public boolean isQueryEmpty(ComponentInstance component, String query) {
+		Model awasModel = AadlHandler.buildAwasModel(Util.getAir(component));
+		SymbolTable symbolTable = SymbolTable.apply(awasModel, new ConsoleTagReporter());
+		FlowGraph<FlowNode, FlowEdge<FlowNode>> flowGraph = FlowGraph.apply(awasModel, symbolTable);
+		return new AwasGraphImpl(flowGraph, symbolTable).queryEvaluator(query).isEmpty();
+	}
+
+	public boolean isQueryNotEmpty(ComponentInstance component, String query) {
+		return !isQueryEmpty(component, query);
+	}
+}
